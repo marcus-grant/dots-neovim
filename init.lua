4c22d6e411fdcac930671e893a8bf1062e96f0d6
Marcus Grant <marcusfg@gmail.com>
2022-09-21 00:13:37 +0200

    DELETEME comments for dynamic lua loader
    * No longer works with updated LUA API
    * Should be deleted if no longer needed
    * Now explicitly adds modules
    * DELETEME delete the code commented out form previous commit
    * FIXME delete the code commented out form previous commit

--- init.lua
+++ init.lua
@@ -6,22 +6,28 @@
 -- My NeoVim configurations, all submodules get loaded here (in-order).
 -- No other logic or configuration should get used here.
 
-function _G.LoadConfigs()
-    local nvim_modules = {
-        'defaults',     -- Should be early
-        'plugins',      -- Should be one of the first
-        'appearance',   -- Should be early, but after defaults
-        'fzf',          -- Should be before keymap
-        'keymap',       -- Keymap should be near the end to override
-    }
-    for _, mod in ipairs(nvim_modules) do
-        -- print(vim.env.HOME .. '/.config/nvim/lua/' .. mod .. '.lua')
-        -- dofile(vim.env.HOME .. '/.config/nvim/lua/' .. mod .. '.lua')
-        local ok, _ = pcall(require, mod)
-        if not ok then
-            error('Error loading ' .. module '\n\n' .. err)
-        end
-    end
-    print('Loaded configs')
-end
-LoadConfigs()
+require('defaults')
+require('plugins')
+require('appearance')
+require('fzf')
+require('keymap')
+
+-- function _G.LoadConfigs()
+    -- local nvim_modules = {
+        -- 'defaults',     -- Should be early
+        -- 'plugins',      -- Should be one of the first
+        -- 'appearance',   -- Should be early, but after defaults
+        -- 'fzf',          -- Should be before keymap
+        -- 'keymap',       -- Keymap should be near the end to override
+    -- }
+    -- for _, mod in ipairs(nvim_modules) do
+        -- -- print(vim.env.HOME .. '/.config/nvim/lua/' .. mod .. '.lua')
+        -- -- dofile(vim.env.HOME .. '/.config/nvim/lua/' .. mod .. '.lua')
+        -- local ok, _ = pcall(require, mod)
+        -- if not ok then
+            -- error('Error loading ' .. module '\n\n' .. err)
+        -- end
+    -- end
+    -- print('Loaded configs')
+-- end
+-- LoadConfigs()
