ceccddc117315c2571554993e5077671dc22052f
Marcus Grant <marcusfg@gmail.com>
2022-12-29 23:55:13 +0100

    Comment out reload config, no work

--- init.lua
+++ init.lua
@@ -11,25 +11,4 @@ require('plugins')
 require('keymap')
 require('appearance')
 require('fzf')
-
--- Taken from
--- https://www.reddit.com/r/neovim/comments/puuskh/how_to_reload_my_lua_config_while_using_neovim/
-function _G.LoadConfigs()
-    local nvim_modules = {
-        'defaults',     -- Should be early
-        'plugins',      -- Should be one of the first
-        'keymap',       -- Keymap should be near the end to override
-        'appearance',   -- Should be early, but after defaults
-        'fzf',          -- Should be before keymap
-    }
-    for _, mod in ipairs(nvim_modules) do
-        -- print(vim.env.HOME .. '/.config/nvim/lua/' .. mod .. '.lua')
-        -- dofile(vim.env.HOME .. '/.config/nvim/lua/' .. mod .. '.lua')
-        local ok, _ = pcall(require, mod)
-        if not ok then
-            error('Error loading ' .. module '\n\n' .. err)
-        end
-    end
-    print('Loaded configs')
-end
-LoadConfigs()
+require('zk').setup()
