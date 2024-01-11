5d82a2ff173c5c4c454b79dfd38087c362a1161d
Marcus Grant <marcusfg@gmail.com>
2024-01-11 15:59:38 +0100

    Add setup for luals & default als lsps

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -24,3 +24,37 @@ require('mason-lspconfig').setup({
         'zk',
     },
 })
+
+-- Taken from https://youtu.be/lpQMeFph1RE?si=B-kJOtJkFyDNLZ4e
+-- TODO: Figure out how to put these in whichkey w/ better binds for action
+local on_attach = function(_, _)
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
+
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+    -- TODO: Find ways to do this with fzf instead or just w/o telescope
+    -- vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+end
+
+-- Taken from https://github.com/neovim/nvim-lspconfig
+local lspCfg = require('lspconfig')
+-- taken from https://github.com/neovim/neovim/issues/21686#issuecomment-1522446128
+lspCfg.als.setup { }
+lspCfg.lua_ls.setup {
+    on_attach = on_attach,
+    settings = {
+        Lua = {
+            runtime = { version = 'LuaJIT' },
+            diagnostics = { globals = { 'vim', 'require' } },
+            workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = { enable = false },
+        }
+    }
+}
+lspCfg.pyright.setup {}
+
+
+
